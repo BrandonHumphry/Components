583a5504746ff06d4785f362f63080abfc318b85
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-18 03:39:06 -0800

    trying to use react router to link pages from navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div
+      style={{
+        padding: "300px",
+        display: "flex",
+        color: "black",
+        alignItems: "center",
+        height: "300px"
+      }}
+    >
+      <h1>home</h1>
+    </div>
+  );
+}
+
+export default Home;
