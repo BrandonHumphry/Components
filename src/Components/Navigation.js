3c7a4246ca130f1d2e5adc9a489910aa43beb1b8
Brandon Humphry <brandonhumphry@gmail.com>
2021-05-11 23:56:50 -0700

    switched toggle to brand name instead of span class which resolved the bug on mobile

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,17 +1,24 @@
 import "./Navigation.css";
 
-function Navigation() {
-  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const openNav = () => {
+  const toggleButton = document.getElementsByClassName("brand-title");
   const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  navbarLinks.classList.toggle("active");
+};
 
-  toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-  });
+function Navigation() {
+  //   const toggleButton = document.getElementsByClassName("toggle-button")[0];
+
+  //   toggleButton.addEventListener("click", () => {
+  //     navbarLinks.classList.toggle("active");
+  //   });
 
   return (
     <div>
       <nav className="navbar">
-        <div className="brand-title">Name</div>
+        <div className="brand-title" onClick={openNav}>
+          Name
+        </div>
 
         <div className="toggle-button">
           <span className="bar"></span>
