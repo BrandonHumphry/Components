151b3c28e46e96e974e965b98feb7559b36ee558
Brandon Humphry <brandonhumphry@gmail.com>
2021-06-17 23:54:50 -0700

    workng with flexbox to adjust gallery Cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,40 +1,18 @@
 import Card from "../Components/Card/Card";
 import "./Portfolio.css";
 
-const Portfolio = ({ data }) => {
-  if (data) {
-    var projects = data.projects.map(function (projects) {
-      var projectImage = "images/portfolio/" + projects.image;
-      return (
-        <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                  <p>{projects.category}</p>
-                </div>
-              </div>
-              <div className="link-icon">
-                <i className="fa fa-link"></i>
-              </div>
-            </a>
-          </div>
-        </div>
-      );
-    });
-  }
-
+const Portfolio = () => {
   return (
     <div>
       <div className="gallery">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card>
+          <p>1</p>
+        </Card>
+        <Card>2</Card>
+        <Card>3</Card>
+        <Card>4</Card>
+        <Card>5</Card>
+        <Card>6</Card>
       </div>
     </div>
   );
