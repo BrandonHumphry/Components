97d77ff87bf93477685dcf63118bb22ff04ba586
Brandon Humphry <brandonhumphry@gmail.com>
2021-06-23 23:48:01 -0700

    setting up pages for selected company/client

---
+++ src/pages/Portfolio/SelectedClient.js
@@ -0,0 +1,20 @@
+import "./SelectedClient.css";
+
+const SelectedClient = () => {
+  return (
+    <div>
+      <img src=".jpg" alt="" />
+      <h1>Title</h1>
+      <h2>SubTitle</h2>
+      <p>Summary</p>
+      <div className="links">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SelectedClient;
