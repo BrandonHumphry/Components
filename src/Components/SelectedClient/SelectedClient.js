a5c8bf2a81f8f799355121a215caf6da71db13ce
Brandon Humphry <brandonhumphry@gmail.com>
2021-08-04 23:13:19 -0700

    adjusting some props and beginning css to style selected client pages

--- src/Components/SelectedClient/SelectedClient.js
+++ src/Components/SelectedClient/SelectedClient.js
@@ -1,37 +1,42 @@
+import "SelectedClient.css";
+import { Link } from "react-router-dom";
 import SelectedClientImage from "./SelectedClientImage";
 
 const SelectedClient = props => {
-  const { id, image } = props;
+  const {
+    id,
+    image,
+    customername,
+    projectname,
+    projectdetails,
+    linkTo,
+    siteName
+  } = props;
   return (
     <div className="projectAbout" key={id}>
-      <img className="projectLogo"image ={image}>
-        <img src="./image.png" alt="alt tag text goes here" />
-      </div>
+      <img className="projectLogo" image={image} />
       <div className="projectDescription">
-        <h1>Customer Name</h1>
-        <h3>Project Name</h3>
-        <p>
+        <h1 className="CustomerName">{customername}</h1>
+        <h3 className="ProjectName">{projectname}</h3>
+        <p className="projectDetails">
+          {projectdetails}
           Project Description Sed ultricies nisi vitae metus consectetur
           molestie. Integer eu dolor nec tortor imperdiet semper. Integer a erat
           mauris. Donec ac dignissim nisl. Suspendisse potenti. Nam cursus dui
           mi, eget scelerisque nunc pretium ut. Aliquam erat volutpat.
           Pellentesque cursus malesuada mi ut tincidunt.
         </p>
-        <a href="https://www.website.com/" rel="noreferrer" target="_blank">
+        <Link to={linkTo}>{siteName}</Link>
+        {/* <a href="https://www.website.com/" rel="noreferrer" target="_blank">
           View Website
-        </a>
+        </a> */}
       </div>
+
       <div className="pictureGallery">
-
-    
-      projectImage={""}
-        clientName="title for card 1"
-        projectName="company 1"
-        projectDescription="Lorem ipsum dolor sit amet, consectetur 
-      adipiscing elit. Nulla lobortis sapien vel suscipit 
-      rutrum."
-        linkTo=
-
+        projectImage={""}
+        clientName="title for card 1" projectName="company 1"
+        projectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Nulla lobortis sapien vel suscipit rutrum." linkTo=
         <SelectedClientImage />
       </div>
     </div>
