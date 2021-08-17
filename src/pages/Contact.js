b8e17d4d350ac829b22d8bd628e60abd3741e2f3
Brandon Humphry <brandonhumphry@gmail.com>
2021-08-17 08:38:47 -1000

    working on contact page, needs styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -29,113 +29,31 @@ const Contact = ({ data }) => {
   };
 
   return (
-    <section id="contact">
-      <div className="row section-head">
-        <div className="two columns header-col">
-          <h1>
-            <span>Get In Touch.</span>
-          </h1>
-        </div>
+    <div className="contactbackground">
+      <div className="contact-container">
+        <h1> I'm a talker</h1>
+        <p id="thankful">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in
+          aliquet turpis, at imperdiet orci. Pellentesque semper erat nisl, ut
+          ultrices lectus efficitur ut. Pellentesque faucibus tellus tristique
+          magna volutpat vestibulum. Fusce id purus malesuada, volutpat libero
+          nec, cursus lorem. Suspendisse mollis ante at elit malesuada, sit amet
+          dignissim ex lobortis. Curabitur rhoncus nulla sed lorem volutpat, id
+          ullamcorper nunc ullamcorper.
+        </p>
 
-        <div className="ten columns">
-          <p className="lead">{contactMessage}</p>
+        <div className="alignbuttons">
+          <a
+            id="button"
+            className="buttons"
+            href="mailto:eandedesignstudio@gmail.com?Subject=StudioEandE.com%20"
+            target="_top"
+          >
+            E - mail Me
+          </a>
         </div>
       </div>
-
-      <div className="row">
-        <div className="eight columns">
-          <form onSubmit={submitForm}>
-            <fieldset>
-              <div>
-                <label htmlFor="contactName">
-                  Name <span className="required">*</span>
-                </label>
-                <input
-                  type="text"
-                  defaultValue=""
-                  value={name}
-                  size="35"
-                  id="contactName"
-                  name="contactName"
-                  onChange={e => setName(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="contactEmail">
-                  Email <span className="required">*</span>
-                </label>
-                <input
-                  type="text"
-                  defaultValue=""
-                  value={email}
-                  size="35"
-                  id="contactEmail"
-                  name="contactEmail"
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="contactSubject">Subject</label>
-                <input
-                  type="text"
-                  defaultValue=""
-                  value={subject}
-                  size="35"
-                  id="contactSubject"
-                  name="contactSubject"
-                  onChange={e => setSubject(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="contactMessage">
-                  Message <span className="required">*</span>
-                </label>
-                <textarea
-                  cols="50"
-                  rows="15"
-                  value={message}
-                  onChange={e => setMessage(e.target.value)}
-                  id="contactMessage"
-                  name="contactMessage"
-                ></textarea>
-              </div>
-
-              <div>
-                <button onClick={submitForm} type="submit" className="submit">
-                  Submit
-                </button>
-              </div>
-            </fieldset>
-          </form>
-
-          <div id="message-warning"> Error boy</div>
-          <div id="message-success">
-            <i className="fa fa-check"></i>Your message was sent, thank you!
-            <br />
-          </div>
-        </div>
-
-        {/* <aside className="four columns footer-widgets">
-          <div className="widget widget_contact">
-            <h4>Address and Phone</h4>
-            <p className="address">
-              {contactName}
-              <br />
-              {contactEmail}
-              <br />
-              <br />
-              {street} <br />
-              {city} {state} {zip}
-              <br />
-              <span>{phone}</span>
-            </p>
-          </div>
-        </aside> */}
-      </div>
-    </section>
+    </div>
   );
 };
 
